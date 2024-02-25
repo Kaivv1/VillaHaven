26650425407e74fb7277c0d75d4561a94fa44988
Kaloyan Vulchev <kvalchevv1@gmail.com>
2024-02-25 15:04:33 +0200

    first commit branch

--- client/src/hooks/useIsMobile.jsx
+++ client/src/hooks/useIsMobile.jsx
@@ -2,25 +2,6 @@ import { useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
 export const useIsMobile = (width) => {
-  // const [isMobile, setIsMobile] = useLocalStorage(
-  //   window.matchMedia(`(max-width:${width})px`).matches,
-  //   "isMobile"
-  // );
-
-  // useEffect(() => {
-  //   const mediaQuery = window.matchMedia(`(max-width:${width}px)`);
-
-  //   const handleMediaQuery = (e) => {
-  //     setIsMobile(e.matches);
-  //   };
-
-  //   mediaQuery.addEventListener("change", handleMediaQuery);
-
-  //   return () => {
-  //     mediaQuery.removeEventListener("change", handleMediaQuery);
-  //     localStorage.removeItem("isMobile");
-  //   };
-  // }, [width, setIsMobile]);
   const [isMobile, setIsMobile] = useLocalStorage(
     window.innerWidth <= width,
     "isMobile"
