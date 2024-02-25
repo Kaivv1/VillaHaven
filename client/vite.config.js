9c2f9d455f0c6181ca1881c1aabacdb3694192da
Kaloyan Vulchev <kvalchevv1@gmail.com>
2024-02-25 16:13:51 +0200

    autoprefixer

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Sass from "sass";
+import Autoprefixer from "autoprefixer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Sass({}), Autoprefixer()],
 });
