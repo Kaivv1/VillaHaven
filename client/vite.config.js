157885e2ca57865366a478a4881a523f9beb79b3
Kaloyan Vulchev <kvalchevv1@gmail.com>
2024-02-25 16:29:23 +0200

    plugin autoprefixer

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  plugins: [react(), Autoprefixer()],
+  plugins: [react()],
 });
